2e57d46d6e5ef82600904ae01eb6fbd4aa6f7cf5
Teemu Partanen <teemu.partanen@hiq.fi>
2022-02-23 10:39:20 +0200

    Initial commit

---
+++ src/main/java/fi/hiq/reference/spring_boot_reference/SpringBootReferenceApplication.java
@@ -0,0 +1,13 @@
+package fi.hiq.reference.spring_boot_reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/hiq/reference/spring_boot_reference/SpringBootReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hiq.reference.spring_boot_reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
