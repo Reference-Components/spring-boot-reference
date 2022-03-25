db41b166520e8599daa1f44f45c13302943defd8
Teemu Partanen <teemu.partanen@hiq.fi>
2022-03-25 09:58:27 +0200

    Dynamically set LDAP server port

--- src/main/java/fi/hiq/reference/spring_boot_reference/config/AuthenticationConfiguration.java
+++ src/main/java/fi/hiq/reference/spring_boot_reference/config/AuthenticationConfiguration.java
@@ -1,5 +1,6 @@
 package fi.hiq.reference.spring_boot_reference.config;
 
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
 import fi.hiq.reference.spring_boot_reference.security.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,8 @@ import javax.annotation.Resource;
 public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
   @Resource
   private UserDetailServiceImpl userDetailsService;
+  @Resource
+  private InMemoryDirectoryServer ldapServer;
   @Value("${referencecomponent.ldap.user-dn-pattern}")
   private String userDnPattern;
   @Value("${referencecomponent.ldap.group-search-base}")
@@ -46,7 +49,8 @@ public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
         .userDnPatterns(userDnPattern)
         .groupSearchBase(groupSearchBase)
         .contextSource()
-        .url(ldapUrl)
+        // Embedded LDAP server uses random port, so replace the port placeholder with correct port
+        .url(ldapUrl.replace("{port}", String.valueOf(ldapServer.getListenPort())))
         .and()
         .passwordCompare()
         .passwordEncoder(passwordEncoder())
